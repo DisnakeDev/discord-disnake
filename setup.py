1d280a9e7545cf469389e32461bab5e116d0d5b3
EQUENOS <akkaunt.vzapase@gmail.com>
2022-01-02 14:03:48 +0300

    Auto requirement

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 import re
-
 from setuptools import setup
 
-requirements = []
-with open("requirements.txt", encoding="utf-8") as f:
-    requirements = f.read().splitlines()
 
 version = ""
 with open("disnake/__init__.py", encoding="utf-8") as f:
@@ -13,25 +9,7 @@ with open("disnake/__init__.py", encoding="utf-8") as f:
 if not version:
     raise RuntimeError("version is not set")
 
-if version.endswith(("a", "b", "rc")):
-    # append version identifier based on commit count
-    try:
-        import subprocess
-
-        p = subprocess.Popen(
-            ["git", "rev-list", "--count", "HEAD"], stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        )
-        out, err = p.communicate()
-        if out:
-            version += out.decode("utf-8").strip()
-        p = subprocess.Popen(
-            ["git", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        )
-        out, err = p.communicate()
-        if out:
-            version += "+g" + out.decode("utf-8").strip()
-    except Exception:
-        pass
+requirements = [f"disnake=={version}"]
 
 readme = ""
 with open("README.md", encoding="utf-8") as f:
