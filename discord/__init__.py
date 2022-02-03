85cacd91d89412b3fe008194c709fc0f7d2f4a87
onerandomusername <genericusername414+git@gmail.com>
2022-02-03 00:58:36 -0500

    fix: make __main__ runnable

--- discord/__init__.py
+++ discord/__init__.py
@@ -1,37 +1,48 @@
-"""
-Discord API Wrapper
-~~~~~~~~~~~~~~~~~~~
+import importlib
+import importlib.abc
+import importlib.machinery
+import importlib.util
+import os
+import sys
 
-A basic wrapper for the Discord API.
-
-:copyright: (c) 2015-present Rapptz
-:license: MIT, see LICENSE for more details.
-
-"""
+MAIN_PACKAGE = "disnake"
 
 
-def _add_import_hook():
-    import importlib
-    import importlib.machinery
-    import importlib.util
-    import sys
+class MainLoader(importlib.abc.ExecutionLoader):
+    def get_filename(self, fullname):
+        spec = importlib.util.find_spec(fullname.split(".")[0])
+        if not spec or not spec.origin:
+            raise ImportError
 
-    class DiscordFinder(importlib.machinery.PathFinder):
-        @classmethod
-        def find_spec(cls, fullname: str, path=None, target=None):
-            """Try to find a spec for 'fullname' on sys.path or 'path'.
-            The search is based on sys.path_hooks and sys.path_importer_cache.
-            """
-            if not fullname.startswith("discord"):
-                return None
-            fullname = fullname.replace("discord", "disnake")
-            return importlib.util.find_spec(fullname)
+        return os.path.dirname(spec.origin) + os.sep + "__main__.py"
+
+    def get_source(self, fullname):
+        if file := self.get_filename(fullname):
+            with open(file, "r") as f:
+                return f.read()
+        raise ImportError
+
+
+class DiscordFinder(importlib.machinery.PathFinder):
+    @classmethod
+    def find_spec(cls, fullname: str, path=None, target=None):
+        """Try to find a spec for 'fullname' on sys.path or 'path'.
+        The search is based on sys.path_hooks and sys.path_importer_cache.
+        """
+        if not fullname.startswith(__name__):
+            return None
+        fullname = fullname.replace(__name__, MAIN_PACKAGE, 1)
+
+        # implement a custom loader for __main__
+        if "__main__" in fullname:
+            return importlib.util.spec_from_loader(fullname, MainLoader())
+        return importlib.util.find_spec(fullname)
+
+
+def add_import_hook():
 
     sys.meta_path.insert(0, DiscordFinder)
-    sys.modules["discord"] = sys.modules.get("disnake") or importlib.import_module("disnake")
+    sys.modules[__name__] = sys.modules.get(MAIN_PACKAGE) or importlib.import_module(MAIN_PACKAGE)
 
 
-_add_import_hook()
-del _add_import_hook
-
-from disnake import *
+add_import_hook()
